7c65f3f3451558028c1004c1fdba907a9af5364e
sarwin-github <dapito.sherwin@yahoo.com>
2018-06-01 20:54:58 +0800

    modified env variable

--- config/mysql-db-context.js
+++ config/mysql-db-context.js
@@ -3,26 +3,23 @@ const mysql   = require('mysql')
 const async   = require('async')
 
 const PRODUCTION_DB = 'books';
-const TEST_DB = 'books-test';
+const TEST_DB       = 'books-test';
 
 const MODE_TEST = 'test';
 const MODE_PRODUCTION = 'dev';
 
-const state = {
-  pool: null,
-  mode: null,
-}
+const state = { pool: null, mode: null }
 
 const connect = function(mode, done) {
 	state.pool = mysql.createPool({
 	    connectionLimit : 100,
-	    host     : 'localhost',
-	    port     : '9080',
-	    user     : 'root',
-	    password : '01610715',
-	    database : 'books',
+	    host     : process.env.dbHost,
+	    port     : process.env.dbPort,
+	    user     : process.env.dbUser,
+	    password : process.env.dbPassword,
+	    database : process.env.databaseName,
 	    debug    :  false
-	})
+	});
 
 	state.mode = mode
 	done()
@@ -48,6 +45,18 @@ module.exports.pickEnv = (env, app) => {
 			  	}
 			})
 	    break;
+
+        case 'test':
+            connect(this.MODE_TEST, function(err) {
+    			if (err) {
+    		    	console.log('Unable to connect to MySQL.')
+    		    	process.exit(1);
+    			} else {
+    				console.log('Connected to MySQL.')
+    		    	app.set('port', process.env.PORT || 9002);
+    		  	}
+    		})
+        break;
 	};
 
 	// Set session and cookie max life, store session in mongo database
